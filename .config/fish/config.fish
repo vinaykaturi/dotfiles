aa6b40e8666ca25e27a3da4fda4cc034baf7d679
vinaykaturi <vinaykaturi99@gmail.com>
2026-02-01 00:56:44 +0530

    edited fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,5 +6,6 @@ fastfetch -c /usr/share/fastfetch/presets/examples/12.jsonc
 alias ls="eza -lh"
 alias la="eza -alh"
 alias cls="clear"
+alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'  
 
 end
